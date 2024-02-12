baba904564fa97375ec42226d39c0f0da060e7bf
Back2Lobby <tayyabisking1101@gmail.com>
2024-02-12 15:30:32 +0500

    added keymap for previewing git changes

--- lua/back2lobby/plugins/git.lua
+++ lua/back2lobby/plugins/git.lua
@@ -4,6 +4,17 @@ return {
         config = function()
             require('gitsigns').setup {
                 attach_to_untracked = true,
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    map('n', 'gp', gs.preview_hunk)
+                end
             }
         end
     }
