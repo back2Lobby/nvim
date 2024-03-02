27e0b7d5b9c5282aec838fc6678e52f67df245db
Back2Lobby <tayyabisking1101@gmail.com>
2024-03-02 16:51:28 +0500

    Fixed incorrect mapping for quickfix list

--- lua/back2lobby/remap.lua
+++ lua/back2lobby/remap.lua
@@ -10,10 +10,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set({"i", "v"}, "<C-l>", "<Esc>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
