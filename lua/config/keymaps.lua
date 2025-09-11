6dc7e29cbf2d135b29d9e2825c212cd6d7aef155
Back2Lobby <tayyabisking1101@gmail.com>
2025-09-11 20:36:45 +0500

    new keybind for explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-m>", vim.cmd.Ex)
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -10,4 +8,6 @@ vim.keymap.set({ "i", "v" }, "<C-l>", "<Esc>")
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
-vim.keymap.set("n", "<CR>", "<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<CR>", "j", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-k>", vim.cmd.Ex, { noremap = true, silent = true })
